d06795f1c14fb5f7ab6e3b0494717d6b0d4a8f2c
JinXin <jin_xin@rd.netease.com>
2015-05-06 19:59:08 +0800

    Initial Commit

---
+++ HuiYing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuiYing
+//
+//  Created by Jin Xin on 15/5/6.
+//  Copyright (c) 2015年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HuiYing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HuiYing
+//
+//  Created by Jin Xin on 15/5/6.
+//  Copyright (c) 2015年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
